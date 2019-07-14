20f64ee2e41b3bab126a91ccfc8b412e80e4287c
Paul Rosero <paul_rosero@yahoo.com>
2019-07-14 14:03:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,26 +60,67 @@ class Student
   end
   
   def self.all_students_in_grade_9
-    self.all.each do |student|
-      binding.pry
-      student.grade = "9"
-    en
+    sql = <<-SQL
+      SELECT COUNT (*)
+      FROM students
+      WHERE grade = 9;
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def self.students_below_12th_grade
-    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12;
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
-  def self.first_X_students_in_grade_10
-    
+  def self.first_X_students_in_grade_10(number_of_students)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, number_of_students).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def self.first_student_in_grade_10
-    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
-  def self.all_students_in_grade_x
-    
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+      ORDER BY students.id 
+      SQL
+      
+      DB[:conn].execute(sql, grade).map do |row|
+        self.new_from_db(row)
+      end
   end
 end
 
